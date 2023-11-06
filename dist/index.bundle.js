f93b46f4e5cc8ce99cd9aea5958ca7e56b66bb29
Greg Schneider <greg53x@msn.com>
2023-11-05 22:28:31 -0700

    function to get random number in range

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -3,6 +3,19 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+//
+
+const points = 0;
+const dicePool = [1, 2, 3, 4, 5, 6];
+
+const rollButton = document.getElementById("rollButton");
+
+function getRandomIntInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+rollButton.addEventListener("click", () => {});
 
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBLDZDQUE2QyIsInNvdXJjZXMiOlsid2VicGFjazovL2RpY2VjcmVtZW50YWwvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy9cclxuXHJcbmNvbnN0IHBvaW50cyA9IDA7XHJcbmNvbnN0IGRpY2VQb29sID0gWzEsIDIsIDMsIDQsIDUsIDZdO1xyXG5cclxuY29uc3Qgcm9sbEJ1dHRvbiA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicm9sbEJ1dHRvblwiKTtcclxuXHJcbmZ1bmN0aW9uIGdldFJhbmRvbUludEluUmFuZ2UobWluLCBtYXgpIHtcclxuICByZXR1cm4gTWF0aC5mbG9vcihNYXRoLnJhbmRvbSgpICogKG1heCAtIG1pbiArIDEpKSArIG1pbjtcclxufVxyXG5cclxucm9sbEJ1dHRvbi5hZGRFdmVudExpc3RlbmVyKFwiY2xpY2tcIiwgKCkgPT4ge30pO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ src/index.js
@@ -0,0 +1,12 @@
+//
+
+const points = 0;
+const dicePool = [1, 2, 3, 4, 5, 6];
+
+const rollButton = document.getElementById("rollButton");
+
+function getRandomIntInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+rollButton.addEventListener("click", () => {});
